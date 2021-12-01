c8aeb249af81b8556fab556b836ad4146c3b9b3e
1+1=100 <95321232+Rubyian@users.noreply.github.com>
2021-11-30 21:10:03 -0300

    Create main.rb

---
+++ main.rb
@@ -0,0 +1,9 @@
+require 'discordrb' #biblioteca requerida. Se necessario escreva em seu terminal "gem install discordrb".
+bot = Discordrb::Commands::CommandBot.new token: "{seu token}", client_id: "{id do seu bot}", prefix: "{seu prefix}" #Informações do seu bot
+
+bot.command :oi do |msg|  #Defina o nome do seu comando"
+    msg.respond "Olá!" "Defina a mensagem do seu comando"
+end
+
+at_exit { bot.stop }
+bot.run
