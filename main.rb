51cba88126e2db91355e4fe5c4c4031c7a0233f2
1+1=100 <95321232+Rubyian@users.noreply.github.com>
2021-11-30 22:28:13 -0300

    Update main.rb

--- main.rb
+++ main.rb
@@ -2,7 +2,7 @@ require 'discordrb' #biblioteca requerida. Se necessario escreva em seu terminal
 bot = Discordrb::Commands::CommandBot.new token: "{seu token}", client_id: "{id do seu bot}", prefix: "{seu prefix}" #Informações do seu bot
 
 bot.command :oi do |msg|  #Defina o nome do seu comando"
-    msg.respond "Olá!" "Defina a mensagem do seu comando"
+    msg.respond "Olá!" #"Defina a mensagem do seu comando"
 end
 
 at_exit { bot.stop }
